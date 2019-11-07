1c983620e34db39d94f7754f870fe5ce21e640f7
Levente Szabados <szablevi@gmail.com>
2019-11-07 12:30:45 +0100

    extension to utf handling

--- shotwell_hack.py
+++ shotwell_hack.py
@@ -4,15 +4,16 @@
 
 import time
 from os.path import expanduser
+import shlex
 
 import subprocess
 
 result = subprocess.run(['xdotool', "search", "--onlyvisible","--class", "Shotwell", 'getwindowname'], stdout=subprocess.PIPE)
 
-window_name = str(result.stdout)
+window_name = result.stdout.decode('utf-8')
 
 result = subprocess.run(['xdotool', 'getactivewindow'], stdout=subprocess.PIPE)
-window_id=str(result.stdout).replace("b'","").replace("\\n'","")
+window_id=str(result.stdout).replace("\\n'","")
 
 if not "Shotwell" in window_name:
     quit()
@@ -31,7 +32,7 @@ root= tk.Tk()
 root.withdraw()
 
 MsgBox = tk.messagebox.askquestion ('Fájl törlése '+window_id,'Biztos benne, hogy törölni szeretné a(z) '+full_filename+' fájlt?',icon = 'warning')
-#MsgBox.pack()
+
 if MsgBox == 'yes':
     result = subprocess.run(["xdotool", "windowminimize", window_id], stdout=subprocess.PIPE)
     time.sleep(0.1)
@@ -39,6 +40,7 @@ if MsgBox == 'yes':
     time.sleep(0.1)
     result = subprocess.run(["xdotool", "key", "space"], stdout=subprocess.PIPE)
     time.sleep(0.3)
+    full_filename = str(shlex.quote(full_filename)).replace("'","")
     result = subprocess.run(["rm",full_filename], stdout=subprocess.PIPE)
     time.sleep(0.1)
     root.destroy()
